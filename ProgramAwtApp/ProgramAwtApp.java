3c68e882fcbcc0d9a64e438ad0f33b1a44b621d4
Nick Zakharov <46914559+Nick-02@users.noreply.github.com>
2019-01-30 18:55:46 +0200

    Update ProgramAwtApp.java

--- ProgramAwtApp/ProgramAwtApp.java
+++ ProgramAwtApp/ProgramAwtApp.java
@@ -18,6 +18,40 @@ class GrLibrary extends Frame implements WindowListener {
         int RH=rc.width;
         int RW=rc.height;
         g.setColor(Color.white);
+
+        Graphics2D g2=(Graphics2D) g;
+        g2.setStroke(new BasicStroke(4.0f));
+
+        int cx,cy,dx,dy;
+
+        cx=RH/2; dx=RH/20;
+        cy=RW/2; dy=RW/20;
+
+        //TODO
+        String txt="Ура! Перший крок зроблено! :)";
+        g.setFont(new Font("Trebuchet Ms",Font.PLAIN,24));
+        g.setColor(Color.BLUE);
+        int n=g.getFontMetrics().stringWidth(txt);
+
+        g.drawString(txt,n/2-150,n/2-100);
+
+        g.setColor(Color.red);
+        for (int i = 1; i <45 ; i++) {
+            g.drawLine(cx-100,cy,cx+200,cy+250-i*10);
+        }
+        g.setColor(Color.GREEN);
+        g.fillRect(40,100,5*dx,100+9*dy);
+
+        g.setColor(Color.CYAN);
+        g.drawRect(30,90,5*dx+20,120+9*dy);
+
+        g.setColor(Color.WHITE);
+        g.fillOval(55,120,4*dx,100+5*dy);
+
+        g.setColor(Color.BLACK);
+        g.drawOval(45,110,5*dx-10,120+5*dy);
+
+
     }
     public GrLibrary(){
         super("Графічні примітиви. Малюємо!");
